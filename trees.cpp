ae6975014e15f6fa5fe35ffaa91b9a993b8c6e8c
MingLeo <31139447+MingLeo@users.noreply.github.com>
2019-03-09 23:03:30 +0800

    names.cpp

---
+++ trees.cpp
@@ -0,0 +1,15 @@
+int main(){
+    char[10] name;
+    int i;
+    int j;
+    
+    names[10] = {"Lisa","Kudrow","Alberta","Canary","Islands","Woopsie","Daisy","Woompa","Loompa","HeyHey"};
+    printf("we are about to initialise 10 names");
+            
+    for (i=0;i<10<i++){
+        printf("Student name %d, %s", i, &names[i]);
+        //scanf("%d",names[i]);
+        i++;
+    }
+
+}
